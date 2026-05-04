9178b4a362a43f044e52a9a8e4a8fc14ba03a0d7
Diogo_Duarte <teclo+teal@disroot.org>
2026-05-04 07:18:14 +0000

    Made it such it doesn't rely on instancing a scene.
    Instantiates the nodes it needs, instead of an external packaged scene.
    Added support for a value suffix.

--- drag_spin_box.gd
+++ drag_spin_box.gd
@@ -4,46 +4,53 @@ class_name DragSpinBox
 
 signal value_changed(value:int)
 
-@export var text : String = "" : set=set_text
-func set_text(val:String):
-	text = val
-	_descript.text = val
+@export var text : String = "" : 
+	set(val):
+		text = val
+		if not is_node_ready():
+			await ready
+		_descript.text = val
 
-@export var min_val : int = 0 : set=set_min_val
-func set_min_val(val:int):
-	min_val = min(val, max_val - 1)  # Ensure that this number is always smaller than max_val
-	set_value(value)
+@export var suffix : String = "" :
+	set(val):
+		suffix = val
+		if not is_node_ready():
+			await ready
+		_suffix.text = val
 
-@export var max_val : int = 100 : set=set_max_val
-func set_max_val(val:int):
-	max_val = max(val, min_val + 1)  # Ensure that this number is always larger than min_val
-	set_value(value)
+@export var min_val : int = 0 : 
+	set(val):
+		min_val = min(val, max_val - 1)  # Ensure that this number is always smaller than max_val
+		value = value
+
+@export var max_val : int = 100 : 
+	set(val):
+		max_val = max(val, min_val + 1)  # Ensure that this number is always larger than min_val
+		value = value
 	
 @export var step_incr : int = 1
-@export var value : int : set=set_value
-func set_value(val:int):
-	val = clampi(val, min_val, max_val)
-	val = snappedi(val, step_incr)
-	value = val
-	_val_label.text = str(val)
+@export var value : int : 
+	set(val):
+		val = clampi(val, min_val, max_val)
+		val = snappedi(val, step_incr)
+		value = val
+		if not is_node_ready():
+			await ready
+		_value.text = str(val)
 
 @export var drag_vertical : bool = false ## Is the user supposed to drag vertically rather than horizontally?
 @export var drag_speed : int = 6 ## the ratio of pixels drag the value increment or decrement.
 
+var _descript := Label.new()
+var _value := Label.new()
+var _suffix := Label.new()
 
-var _descript : Label
-var _val_label : Label
 func _init() -> void:
-	var scene = preload("drag_spin_box.tscn").instantiate()
-	add_child(scene)
-	scene.owner = self
-	
-	_descript = scene.get_node("%Descript")
-	_val_label = scene.get_node("%Val_Label")
-
-	_descript.text = text
-	_val_label.text = str(value)
-
+	_descript.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	_value.text = "0"
+	add_child(_descript)
+	add_child(_value)
+	add_child(_suffix)
 
 var is_dragging : bool
 var drag_start : float
@@ -55,7 +62,6 @@ func _gui_input(event: InputEvent) -> void:
 		drag_vect = inverse_lerp(0, 100, drag_speed) * drag_vect * [1, -1][int(drag_vertical)]
 		value = snappedi(drag_vect + val_start, step_incr)
 		value = clamp(value, min_val, max_val)
-		_val_label.text = str(value)
 	
 	if event is InputEventMouseButton and not event.is_echo():
 		is_dragging = event.is_pressed()
